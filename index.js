81c283c6809b7502c97d7a0b68810c965ed84677
Mallory Taylor <malloryataylor13@gmail.com>
2023-04-03 01:00:19 -0400

    Using the reduce method to return a sum of values in an array

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+let total = 0;
+const totalBatteries = batteryBatches.reduce((accumulator, currentValue) => accumulator + currentValue, total);
+totalBatteries;
